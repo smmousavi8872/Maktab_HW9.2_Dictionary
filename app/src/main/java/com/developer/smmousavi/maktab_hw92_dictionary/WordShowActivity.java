16e4ea04d5afa764b37938d81c7820976c5664f5
Mohsen <smmousavi.developer@gmail.com>
2018-08-01 19:44:37 +0430

    Project Created

---
+++ app/src/main/java/com/developer/smmousavi/maktab_hw92_dictionary/WordShowActivity.java
@@ -0,0 +1,13 @@
+package com.developer.smmousavi.maktab_hw92_dictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordShowActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_word_show);
+  }
+}
